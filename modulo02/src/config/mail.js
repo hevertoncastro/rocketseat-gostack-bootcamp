40ad4f588d60b07afa29e0ee84811d4059c4d65c
Heverton Castro <hevertonwebdev@gmail.com>
2020-02-17 08:03:36 -0300

    Envio de e-mail com Nodemailer e Mailtrap

---
+++ modulo02/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: 'b8917e267c2f5c',
+    pass: '124f251b6c4d50',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com',
+  },
+};
